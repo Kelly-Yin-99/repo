f93d456e69cfac6748ed8db6006b98b5c7831d20
Kelly Yin <kellyyin334@gmail.com>
2026-04-13 01:53:59 +0000

    Add CSV files

--- scripts/fetch-csvs.sh
+++ scripts/fetch-csvs.sh
@@ -2,21 +2,19 @@
 
 set -e
 
-# make temp dir
+cd /workspace/repo
+
 tmpdir=$(mktemp -d)
-
-# download zip 
-wget -O "$tmpdir/data.zip" https://raw.githubusercontent.com/joachimvandekerckhove/cogs205b-s26/main/modules/02-version-control/files/data.zip
-
-# unzip
-unzip "$tmpdir/data.zip" -d "$tmpdir"
-
-# get today's date
 today=$(date +%F)
 
-mkdir -p "/workspace/repo/data/$today"
-find "$tmpdir" -maxdepth 1 -type f -name "*.csv" -exec cp {} "/workspace/repo/data/$today/" \;
-cd /workspace/repo
-git add data scripts
+wget -O "$tmpdir/data.zip" "https://raw.githubusercontent.com/joachimvandekerckhove/cogs205b-s26/main/modules/02-version-control/files/data.zip"
+
+unzip "$tmpdir/data.zip" -d "$tmpdir"
+
+mkdir -p "data/$today"
+
+find "$tmpdir" -maxdepth 1 -type f -name "*.csv" -exec cp {} "data/$today/" \;
+
+git add scripts/fetch-csvs.sh "data/$today"
 git commit -m "Add CSV files"
 git push
